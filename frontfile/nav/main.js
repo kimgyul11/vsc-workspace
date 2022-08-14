3dd9f5a547dc3509d8edc63d8778b2e648e83da4
kimgiyul <coloo88@naver.com>
2022-08-14 21:28:25 +0900

    commit

--- frontfile/nav/main.js
+++ frontfile/nav/main.js
@@ -1,6 +1,6 @@
-const toggleBtn = document.querySelector('.navbar_toggle');
-const menu = document.querySelector('.navmenu');
-const icons = document.querySelector('.navright');
+const toggleBtn = document.querySelector('.navbar_togglebtn');
+const menu = document.querySelector('.navbar_menu');
+const icons = document.querySelector('.navbar_linkicon');
 
 toggleBtn.addEventListener('click', () => {
     menu.classList.toggle('active');
