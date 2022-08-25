5c970a0a8b6fca1c1b55c63b79cb9e84de0c3a1f
kimgyul11 <coloo88@naver.com>
2022-08-25 10:06:13 +0900

    c

--- AirLanguage/js/rensyu.js
+++ AirLanguage/js/rensyu.js
@@ -6,12 +6,21 @@ var dd_main = document.querySelector(".dd_main");
 
 
     // 서치박스
-    $(".default_option").click(function(){
-        $(".dropdown ul").addClass("active");
-    });
+    // $(".default_option").click(function(){
+    //     $(".dropdown ul").addClass("active");
+    // });
     
-    $(".dropdown ul li").click(function(){
-        var text = $(this).text();
-        $(".default_option").text(text);
-        $(".dropdown ul").removeClass("active");
-    });
+    // $(".dropdown ul li").click(function(){
+    //     var text = $(this).text();
+    //     $(".default_option").text(text);
+    //     $(".dropdown ul").removeClass("active");
+    // });
+
+const toggleBtn = document.querySelector('.default_option');
+const menu = document.querySelector('.dropdown ul');
+const icons = document.querySelector('.navbar_linkicon');
+
+toggleBtn.addEventListener('click', () => {
+    menu.classList.toggle('active');
+    icons.classList.toggle('active');
+});
